41fed7bad5cd9f0afc06cffbe282317644265413
camilaalvinhao <camila.alvinhao@nubank.com.br>
2021-05-14 14:32:22 -0300

    Desafio primeira semana

---
+++ src/primeira_semana/core.clj
@@ -0,0 +1,78 @@
+(ns primeira-semana.core)
+
+(def cliente {
+              :nome "Jose"
+              :cpf "1010101010"
+              :email "jose@jose.com"
+              :cartao {
+                       :numero "0123456789"
+                       :cvv "123"
+                       :validade "11-05-2030"
+                       :limite 5000
+                       }
+              :compras-realizadas [
+                          {:data            "11/01/2021"
+                              :valor           10.8
+                              :estabelecimento "Supermercado BH"
+                              :categoria       "supermercado"
+                           }
+                          {
+                              :data            "10/01/2021"
+                              :valor           18.5
+                              :estabelecimento "Epa"
+                              :categoria       "supermercado"
+                          }
+                          {
+                              :data            "12/01/2021"
+                              :valor           15.7
+                              :estabelecimento "Araujo"
+                              :categoria       "farmacia"
+                           }
+                         {
+                              :data            "18-01-2021"
+                              :valor           9.2
+                              :estabelecimento "Indiana"
+                              :categoria       "farmacia"
+                              }
+                         {
+                              :data            "23-01-2021"
+                              :valor           39.8
+                              :estabelecimento "Superpao"
+                              :categoria       "padaria"
+                              }
+                         ]
+              })
+
+(defn imprime-compra [valor]
+  (println "\nData:" (-> valor :data)
+           "\nValor: R$" (-> valor :valor)
+           "\nEstabelecimento: " (-> valor :estabelecimento))
+  )
+
+(defn imprime-categorias [categoria]
+  (println "\n Categoria:" (-> categoria :categoria)
+           "\n Total Gasto: R$" (-> categoria :total-gasto))
+  )
+
+
+(defn totaliza-por-categoria [[categoria itens]]
+  {:categoria categoria
+   :total-gasto (->> itens
+                  (map #(:valor % 0))
+                  (reduce +))}
+)
+
+(defn executa-categorizacao []
+  (map totaliza-por-categoria
+      (group-by :categoria
+      (-> cliente :compras-realizadas))))
+
+(println "COMPRAS")
+(println (map imprime-compra (-> cliente :compras-realizadas)))
+
+(println)
+
+(println "TOTAL POR CATEGORIA")
+(println (map imprime-categorias (executa-categorizacao)))
+
+
---
+++ test/primeira_semana/core_test.clj
@@ -0,0 +1,7 @@
+(ns primeira-semana.core-test
+  (:require [clojure.test :refer :all]
+            [primeira-semana.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
